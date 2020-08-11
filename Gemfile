cb88097353b13363539917f0cb5f877eae15f996
Mitch <mitch.robinson97@gmail.com>
2020-08-11 14:19:50 +0100

    Remove everything apart from the app engine exec container

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem "minitest", "~> 5.11"
-gem "minitest-focus", "~> 1.1"
-gem "minitest-rg", "~> 5.2"
-gem "rake", "~> 12.0"
--- test/sample_apps/rails4_app/app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    unless Rails.env == "production"
-      raise "Wrong Rails environment: #{Rails.env}"
-    end
-    render :text => "Hello World!"
-  end
-end
